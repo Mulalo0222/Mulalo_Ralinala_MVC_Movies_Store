0e00368f71fdbe55f0964056a32ce83302b7516f
Mulalo Ralinala <47423852+Mulalo0222@users.noreply.github.com>
2021-12-17 11:00:03 +0200

    first commit

---
+++ Mulalo_Ralinala_MVC_Movies_Store/MvcMovies/Data/MvcMoviesContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MvcMovies.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace MvcMovies.Data
+{
+    public class MvcMoviesContext : DbContext
+    {
+        public MvcMoviesContext(DbContextOptions<MvcMoviesContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Movie> Movie { get; set; }
+    }
+
+    }
